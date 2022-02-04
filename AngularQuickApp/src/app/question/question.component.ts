3f87c991325f52ad97d856ce147c40dffaabc3c4
thuynguyen <thuynt@consultindochina.com>
2022-02-05 00:55:37 +0700

    thuy nguyen commit

---
+++ AngularQuickApp/src/app/question/question.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
+})
+export class QuestionComponent implements OnInit {
+
+  public name: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.name = localStorage.getItem('name')!;
+  }
+
+}
