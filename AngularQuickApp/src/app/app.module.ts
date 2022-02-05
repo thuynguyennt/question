a1e3e99372c818acb883a0f5fc8a008e7635fe36
thuynguyen <thuynt@consultindochina.com>
2022-02-06 01:00:43 +0700

    question quiz

--- AngularQuickApp/src/app/app.module.ts
+++ AngularQuickApp/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { QuestionComponent } from './question/question.component';
 import { HeaderComponent } from './header/header.component';
+import { HttpClientModule} from '@angular/common/http';
+import { ChangebgDirective } from './changebg.directive'
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     QuestionComponent,
-    HeaderComponent
+    HeaderComponent,
+    ChangebgDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
